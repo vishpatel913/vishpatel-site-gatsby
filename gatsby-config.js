2a5924ad8e09e7218f71cc283445ccb7f89d8faf
Vish Patel <vishpatel913@googlemail.com>
2018-08-17 16:11:56 +0800

    initGatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: 'VishPatel.com',
+  },
+  pathPrefix: `/vishpatel913.github.io`,
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
+}
