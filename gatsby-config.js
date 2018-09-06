8948194f87dbf96903955e1acef3f923dd0150e2
Vish Patel <vishpatel913@googlemail.com>
2018-09-06 15:35:44 +0100

    set up gatsby with contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,21 @@ module.exports = {
     title: 'VishPatel.com',
   },
   pathPrefix: `/`,
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `qcrphhesuv4n`,
+        accessToken: `53275fa1db2534ec36b9dd363f767359773ce40ef008ef519b32e5d29574b83e`,
+      },
+    },
+  ],
 }
