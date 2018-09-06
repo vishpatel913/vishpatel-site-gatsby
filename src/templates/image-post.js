adbc68c16ba786a0df27cb5efc0d93fc0b2cb994
Vish Patel <vishpatel913@googlemail.com>
2018-09-06 17:08:27 +0100

    created base template for single post

--- src/templates/image-post.js
+++ src/templates/image-post.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
+import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+
+import colors from '../utils/colors'
 
 const PostContainer = styled.div`
   width: 100%;
@@ -30,6 +33,65 @@ const ContentContainer = styled.div`
     max-width: 100%;
   }
 `
+
+const ImageTitle = styled.h1``
+
+const ImageCaption = styled.p``
+
+const ImageMetaContainer = styled.div`
+  color: grey;
+
+  &:before {
+    content: '';
+    display: block;
+    width: 48px;
+    height: 2px;
+    background: #ddd;
+    margin-bottom: 1rem;
+  }
+`
+
+const DateText = styled.span`
+  display: block;
+  margin-bottom: 4px;
+`
+
+const CategoryLink = styled(Link)`
+  display: block;
+  margin-bottom: 4px;
+  text-decoration: none;
+  color: grey;
+
+  &:hover {
+    color: ${colors.primary};
+  }
+`
+
+const TagLink = styled.span`
+  display: inline-block;
+  text-decoration: none;
+  color: grey;
+  margin-right: 8px;
+
+  &:hover {
+    color: ${colors.primary};
+  }
+`
+
+const Category = ({ title }) => {
+  const text = title.slice(0, 1).toUpperCase() + title.slice(1)
+  return <CategoryLink to={'work/' + title}>{text}</CategoryLink>
+}
+
+const Tag = ({ title }) => {
+  let tagSlug = title
+    .toLowerCase()
+    .split(' ')
+    .join('-')
+  return <TagLink to={'tag/' + tagSlug}>#{title}</TagLink>
+}
+
+const renderTags = tags => {}
 
 const ImageTemplate = ({ data }) => {
   const {
@@ -60,8 +122,12 @@ const ImageTemplate = ({ data }) => {
       </ImageContainer>
       <ContentContainer>
         <h1>{title}</h1>
-        <p>{metaDescription}</p>
-        <p>{dateCreated}</p>
+        {imageCaption && <p>{metaDescription}</p>}
+        <ImageMetaContainer>
+          <DateText>{dateCreated}</DateText>
+          <Category title={category} />
+        </ImageMetaContainer>
+        {tags.sort().map(tag => <Tag key={tag} title={tag} />)}
       </ContentContainer>
     </PostContainer>
   )
