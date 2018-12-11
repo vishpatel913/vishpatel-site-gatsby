bd07b40afaef87e86165f3ad66b8ccf7655ff92e
Vish Patel <vpatel@and.digital>
2018-12-11 16:49:58 +0000

    linted utils folder

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,26 +1,24 @@
-export const capitalizeString = string => {
-  return string
-    .split(/\s|-/)
-    .map(s => s.charAt(0).toUpperCase() + s.substring(1))
-    .join(' ')
-}
+export const capitalizeString = string => string
+  .split(/\s|-/)
+  .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+  .join(" ");
 
 export const getAltText = (title, category) => {
   switch (category) {
-    case 'development':
-      return 'Screenshot of ' + title
-    case 'design':
-      return 'Design titled ' + title
-    case 'photography':
-      return 'Photograph titled ' + title
+    case "development":
+      return `Screenshot of ${title}`;
+    case "design":
+      return `Design titled ${title}`;
+    case "photography":
+      return `Photograph titled ${title}`;
     default:
-      return title
+      return title;
   }
-}
+};
 
-export const editTracedSvg = fluid => {
-  const color = '#311B9255'
-  let newFluid = fluid
-  newFluid.tracedSVG = fluid.tracedSVG.replace('lightgray', color)
-  return newFluid
-}
+export const editTracedSvg = (fluid) => {
+  const color = "#311B9255";
+  const newFluid = fluid;
+  newFluid.tracedSVG = fluid.tracedSVG.replace("lightgray", color);
+  return newFluid;
+};
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,43 +1,43 @@
-import Typography from 'typography'
-import colors from './colors'
+import Typography from "typography";
+import colors from "./colors";
 
 const typography = new Typography({
-  baseFontSize: '18px',
+  baseFontSize: "18px",
   googleFonts: [
     {
-      name: 'Raleway',
-      styles: ['200', '400', '700'],
+      name: "Raleway",
+      styles: ["200", "400", "700"]
     },
     {
-      name: 'Open Sans',
-      styles: ['400', '700'],
-    },
+      name: "Open Sans",
+      styles: ["400", "700"]
+    }
   ],
-  headerFontFamily: ['Raleway', 'sans-serif'],
-  bodyFontFamily: ['Raleway', 'sans-serif'],
+  headerFontFamily: ["Raleway", "sans-serif"],
+  bodyFontFamily: ["Raleway", "sans-serif"],
   bodyWeight: 400,
   headerWeight: 700,
   lightWeight: 200,
   boldWeight: 700,
   overrideStyles: () => ({
     body: {
-      background: colors.background,
+      background: colors.background
     },
     p: {
-      marginBottom: '1rem',
+      marginBottom: "1rem"
     },
     img: {
-      margin: '0',
+      margin: "0"
     },
     a: {
-      textDecoration: 'none',
-      color: colors.primary,
+      textDecoration: "none",
+      color: colors.primary
     },
-    'a:hover': {
-      color: colors.primaryLight,
-    },
-  }),
-})
+    "a:hover": {
+      color: colors.primaryLight
+    }
+  })
+});
 
-const { rhythm, scale } = typography
-export { rhythm, scale, typography as default }
+const { rhythm, scale } = typography;
+export { rhythm, scale, typography as default };
