5a359eeaf6184a8db593d1403001e4ac94ef81cf
Vish Patel <vpatel@and.digital>
2018-12-11 15:41:53 +0000

    removed extra vars

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,5 @@
-let activeEnv = process.env.ACTIVE_ENV || process.env.NODE_ENV || 'development'
-
-console.log(`Using environment config: '${activeEnv}'`)
-
 require('dotenv').config({
-  path: `.env.${activeEnv}`,
+  path: `.env`,
 })
 
 module.exports = {
