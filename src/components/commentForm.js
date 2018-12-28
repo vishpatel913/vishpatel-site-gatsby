99b0918c3dfc85292f27226efca898b4a24a0da0
Vish Patel <vishpatel913@gmail.com>
2018-12-28 15:33:52 +0000

    added the hidden bot field

--- src/components/commentForm.js
+++ src/components/commentForm.js
@@ -51,6 +51,7 @@ const CommentForm = ({ slug }) => {
     <Container>
       <h2>Leave a Comment</h2>
       <form name={formId} method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="bot-field" />
         <input name="form-name" type="hidden" value={formId} />
         <input name="slug" type="hidden" value={slug} />
         <FormInput name="name" type="text" placeholder="Name*" required />
