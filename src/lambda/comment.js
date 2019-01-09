0b66ebc9ae8c65ff2fd8cafef49f6b3e864e6d62
Vish Patel <vpatel@and.digital>
2019-01-09 10:13:52 +0000

    further testing the form

--- src/lambda/comment.js
+++ src/lambda/comment.js
@@ -1,15 +1,17 @@
-const axios = require("axios");
 
 exports.handler = (event, context, callback) => {
-  const { name, email, message } = JSON.parse(event.body);
+  const { name, email, message, slug } = JSON.parse(event.body);
+
+  alert(`Thank you ${name} for commenting on ${slug}`);
+  console.log("LOG", `Thank you ${name} for commenting on ${slug}`);
 
   // axios
   //   .get("https://api.github.com/repos/vishpatel913/vishpatel-site-gatsby/contents/_data/comments")
   //   .then((res) => {
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({ successMsg: `Thanks for commenting, ${name}` })
-  });
+  // callback(null, {
+  //   statusCode: 200,
+  //   body: JSON.stringify({ successMsg: `Thanks for commenting, ${name}` })
+  // });
   // })
   // .catch((err) => {
   //   callback(err);
---
+++ src/lambda/test.js
@@ -0,0 +1,9 @@
+
+exports.handler = (event, context, callback) => {
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({ successMsg: `Lambda function works` })
+  });
+
+};
