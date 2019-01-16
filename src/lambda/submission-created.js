25aadcb132a24fbe15eb442a6c98c553ce1c13b8
Vish Patel <vishpatel913@gmail.com>
2019-01-16 21:01:17 +0000

    renamed function to be called on form submit

---
+++ src/lambda/submission-created.js
@@ -0,0 +1,38 @@
+const contentful = require("contentful-management");
+
+require("dotenv").config({
+  path: ".env"
+});
+
+exports.handler = (event, context, callback) => {
+  const {
+    name, email, message, slug
+  } = JSON.parse(event.body);
+
+  const client = contentful.createClient({
+    accessToken: process.env.CONTENTFUL_MANAGEMENT_ACCESS_TOKEN
+  });
+
+  client
+    .getSpace(process.env.CONTENTFUL_SPACE_ID)
+    .then(space => space.createEntry("postComment", {
+      fields: {
+        postSlug: {
+          "en-US": slug
+        },
+        name: { "en-US": name },
+        email: { "en-US": email },
+        message: {
+          "en-US": message
+        },
+        timestamp: { "en-US": Math.round(new Date().getTime() / 1000) }
+      }
+    }))
+    .then(entry => console.log(entry))
+    .catch(console.error);
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({ successMsg: "Comment saved as draft" })
+  });
+};
