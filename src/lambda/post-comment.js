0fb343da05ab386f763560466e13a5b5a5447887
Vish Patel <vishpatel913@gmail.com>
2019-02-02 20:00:01 +0000

    added validation of production env to lambda fnction

--- src/lambda/post-comment.js
+++ src/lambda/post-comment.js
@@ -1,5 +1,8 @@
 const contentful = require("contentful-management");
 
+const activeEnv = process.env.ACTIVE_ENV || process.env.NODE_ENV || "development";
+console.log(`Using environment config: '${activeEnv}'`);
+
 require("dotenv").config({
   path: ".env"
 });
@@ -14,24 +17,25 @@ exports.handler = (event, context, callback) => {
     accessToken: process.env.CONTENTFUL_MANAGEMENT_ACCESS_TOKEN
   });
 
-  // TODO: add / validation check for slug
-  client
-    .getSpace(process.env.CONTENTFUL_SPACE_ID)
-    .then(space => space.createEntry("postComment", {
-      fields: {
-        postSlug: {
-          "en-US": slug
-        },
-        name: { "en-US": name },
-        email: { "en-US": email },
-        message: {
-          "en-US": message
-        },
-        timestamp: { "en-US": Math.round(new Date().getTime() / 1000) }
-      }
-    }))
-    .then(entry => console.log(entry))
-    .catch(console.error);
+  if (activeEnv === "production") {
+    client
+      .getSpace(process.env.CONTENTFUL_SPACE_ID)
+      .then(space => space.createEntry("postComment", {
+        fields: {
+          postSlug: {
+            "en-US": slug
+          },
+          name: { "en-US": name },
+          email: { "en-US": email },
+          message: {
+            "en-US": message
+          },
+          timestamp: { "en-US": Math.round(new Date().getTime() / 1000) }
+        }
+      }))
+      .then(entry => console.log(entry))
+      .catch(console.error);
+  }
 
   callback(null, {
     statusCode: 200,
