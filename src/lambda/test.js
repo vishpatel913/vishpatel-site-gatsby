ddda8387d96813bd324ecbfd390781cd905b14a3
Vish Patel <vishpatel913@gmail.com>
2019-11-05 18:34:02 +0000

    removed unused files

--- src/lambda/test.js
+++
@@ -1,8 +0,0 @@
-exports.handler = (event, context, callback) => {
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      successMsg: "Test response from lambda function"
-    })
-  });
-};
