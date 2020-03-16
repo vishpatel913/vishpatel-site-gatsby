14740c4ae7ed5d29a35e197d2b28b8722ca8131c
Vish Patel <vishpatel913@gmail.com>
2020-03-16 21:06:03 +0000

    added invert dark move filter to 404

--- src/components/notFoundMessage.js
+++ src/components/notFoundMessage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { connect } from "react-redux";
 import vector from "../../static/images/404-vector.svg";
 
 const Container = styled.div`
@@ -7,15 +8,18 @@ const Container = styled.div`
 `;
 
 const NotFoundVector = styled.img`
+  ${({ invert }) => invert && "filter: invert(1);"}
   width: 500px;
 `;
 
-const NotFoundMessage = () => (
+const NotFoundMessage = ({ isDarkMode }) => (
   <Container>
     <h1>NOT FOUND</h1>
-    <NotFoundVector src={vector} />
+    <NotFoundVector invert={isDarkMode} src={vector} />
     <p>You just found a page that doesn&#39;t exist... oh the sadness.</p>
   </Container>
 );
 
-export default NotFoundMessage;
+const mapStateToProps = ({ isDarkMode }) => ({ isDarkMode });
+
+export default connect(mapStateToProps, null)(NotFoundMessage);
