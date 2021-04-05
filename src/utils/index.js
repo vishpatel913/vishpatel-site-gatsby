8a0e53d8dd2e736894ad0441e54572c148e88ee7
Vish Patel <vishpatel913@gmail.com>
2021-04-05 15:19:39 +0100

    handled svg error for preview

--- src/utils/index.js
+++ src/utils/index.js
@@ -19,10 +19,10 @@ export const getAltText = (title, category) => {
   }
 };
 
-export const editTracedSvg = (fluid, isDark = false) => {
-  const color = theme[isDark ? "dark" : "light"].secondary.slice(1);
-  // const color = `${theme.dark.color.primary.slice(1)}80`;
-  const newFluid = fluid;
-  newFluid.tracedSVG = fluid.tracedSVG?.replace("d3d3d3", color);
-  return newFluid;
-};
+export const editTracedSvg = (fluid, isDark = false) => ({
+  ...fluid,
+  tracedSVG: fluid?.tracedSVG?.replace(
+    "d3d3d3",
+    theme[isDark ? "dark" : "light"].secondary.slice(1)
+  )
+});
