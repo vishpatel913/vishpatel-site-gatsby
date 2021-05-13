d11f0459f06630435b8007d52ed3ef0d0ef68f66
Vish Patel <vishpatel913@gmail.com>
2021-05-13 13:43:43 +0100

    removed unnecc tests

--- src/__tests__/imageGrid.test.js
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-// import renderWithTheme from "./utils/test-renderer";
-// import ImageGrid from "../components/imageGrid";
-
-describe("ImageGrid", () => {
-  it("renders data correctly", () => {
-    expect(true).toBeTruthy();
-  });
-});
