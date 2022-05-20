892a92265860ab38285e4011a0e90b8a3184adbe
Vish Patel <vpatel@and.digital>
2022-05-20 13:50:13 +0100

    fixed markdown renderer component

--- src/components/markdownRenderer.js
+++ src/components/markdownRenderer.js
@@ -1,20 +1,30 @@
 import React from "react";
+import styled from "styled-components";
 import ReactMarkdown from "react-markdown";
 import Link from "./link";
 
-const LinkRenderer = ({ href, title, children }) => (
+const HrComponent = styled.hr`
+  background: ${({ theme }) => theme.color.secondary};
+  margin-bottom: 1rem;
+`;
+
+const LinkComponent = ({ href, title, children }) => (
   <Link href={href} title={title} external={href.indexOf("/") !== 0}>
     {children}
   </Link>
 );
 
-const MarkdownRenderer = ({ source }) => (
-  <ReactMarkdown
-    source={source}
-    renderers={{
-      link: LinkRenderer
-    }}
-  />
-);
+const MarkdownRenderer = ({ source }) => {
+  return (
+    <ReactMarkdown
+      components={{
+        link: LinkComponent,
+        hr: HrComponent
+      }}
+    >
+      {source}
+    </ReactMarkdown>
+  );
+};
 
 export default MarkdownRenderer;
